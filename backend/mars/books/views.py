75ea7c1d7876236ac28f7bac440ad3ec0e11c0e1
krbtsv <maxim.karabtsov@gmail.com>
2022-09-27 23:57:15 +0300

    Create Serializer, views

--- backend/mars/books/views.py
+++ backend/mars/books/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from books.models import Book
+from books.serializers import BooksSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BooksSerializer
+
+
+
