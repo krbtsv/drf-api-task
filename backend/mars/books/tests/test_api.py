87fdc90629c1284a36ee5c74f89eee5e5a69ff2b
krbtsv <maxim.karabtsov@gmail.com>
2022-09-28 05:13:41 +0300

    Create tests for CRUD

--- backend/mars/books/tests/test_api.py
+++ backend/mars/books/tests/test_api.py
@@ -1,3 +1,6 @@
+import json
+
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -8,6 +11,7 @@ from books.serializers import BooksSerializer
 
 class BooksAPITestCase(APITestCase):
     def setUp(self):
+        self.user = User.objects.create(username='test_username')
         self.book_1 = Book.objects.create(title='test book 1', price='10', author_name='author 1')
         self.book_2 = Book.objects.create(title='test book 2', price='35', author_name='author 2')
         self.book_3 = Book.objects.create(title='test book author 1', price='25', author_name='author 3')
@@ -16,6 +20,13 @@ class BooksAPITestCase(APITestCase):
         url = reverse('book-list')
         response = self.client.get(url)
         serializer_data = BooksSerializer([self.book_1, self.book_2, self.book_3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_get_id(self):
+        url = reverse('book-detail', args=(self.book_1.id,))
+        response = self.client.get(url)
+        serializer_data = BooksSerializer(self.book_1).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
@@ -39,3 +50,41 @@ class BooksAPITestCase(APITestCase):
         serializer_data = BooksSerializer([self.book_1, self.book_3, self.book_2], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
+
+    def test_create(self):
+        self.assertEqual(3, Book.objects.all().count())
+        url = reverse('book-list')
+        data = {
+            "title": "Python",
+            "price": 200,
+            "author_name": "Chak Zero"
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.post(url, data=json_data,
+                                    content_type='application/json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(4, Book.objects.all().count())
+
+    def test_update(self):
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            "title": self.book_1.title,
+            "price": 190,
+            "author_name": self.book_1.title
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.put(url, data=json_data,
+                                   content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.book_1 = Book.objects.get(id=self.book_1.id)
+        self.assertEqual(190, self.book_1.price)
+
+    def test_delete(self):
+        self.assertEqual(3, Book.objects.all().count())
+        url = reverse('book-detail', args=(self.book_1.id,))
+        self.client.force_login(self.user)
+        response = self.client.delete(url)
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+        self.assertEqual(2, Book.objects.all().count())
--- backend/mars/mars/settings.py
+++ backend/mars/mars/settings.py
@@ -112,9 +112,10 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        # 'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
 }
