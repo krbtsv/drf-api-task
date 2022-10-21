849b2a2a309164045dafa86d61fed9d20954572c
krbtsv <maxim.karabtsov@gmail.com>
2022-10-21 17:24:17 +0300

    Using select_related, prefetch_related for optimization query

--- backend/mars/books/views.py
+++ backend/mars/books/views.py
@@ -14,7 +14,7 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all().annotate(
         annotated_likes=Count(Case(When(userbookrelation__like=True, then=1))),
         rating=Avg('userbookrelation__rate')
-    ).order_by('id')
+    ).select_related('creator').prefetch_related('readers').order_by('id')
     serializer_class = BooksSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     permission_classes = [IsCreatorOrStaffOrReadOnly]
